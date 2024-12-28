d4ead1b4c21d6e6557f8b0407959210f6e9c22cd
JISHAN <jahedulislamdev@gmail.com>
2024-12-28 15:20:39 +0600

    make private and updated firebase config

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
+// https://firebase.google.com/docs/web/setup#available-librarie
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCsY9cHdCAygSmEf-E2yMfoFoM0MC4igV8",
-    authDomain: "dragon-news-auth-3b45d.firebaseapp.com",
-    projectId: "dragon-news-auth-3b45d",
-    storageBucket: "dragon-news-auth-3b45d.firebasestorage.app",
-    messagingSenderId: "726890272136",
-    appId: "1:726890272136:web:c0d576c8f0f336164b163d",
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
